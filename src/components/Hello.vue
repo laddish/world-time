038f0b3d3e04f4b1e974505f39ee38f9532ea371
laddish <laddish@foxmail.com>
2022-06-13 14:27:06 +0800

    feat: setup unocss

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -8,7 +8,10 @@ const uppercase = computed(() => props.name.toUpperCase());
 
 <template>
   Hello
-  <div>
+  <div  text-green font="sans bold">
     {{ uppercase }}
+  </div>
+  <div i-ion:accessibility text-5xl text-gray>
+
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from "unplugin-vue-components/vite"
 import AutoImport from "unplugin-auto-import/vite"
+import UnoCSS from "unocss/vite"
+import { presetAttributify, presetIcons, presetUno } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +15,16 @@ export default defineConfig({
       imports: [
         "vue"
       ]
-    })
+    }),
+    UnoCSS(
+      {
+        presets: [
+          presetUno(),
+          presetAttributify(),
+          presetIcons()
+        ]
+      }
+    ),
   ],
 
 })
